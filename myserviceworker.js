b4f6852721dd3511e86963b642868fed359bf7b6
Lontchi12 <lontchilionelle@gmail.com>
2020-10-06 11:56:48 +0100

    updates on service worker

--- myserviceworker.js
+++ myserviceworker.js
@@ -3,8 +3,20 @@ const assets = [
     "/",
     "/index.html",
     "/css/styles.css",
-    "/js/script.js",
-    "/assets/img/**",
+    "/js/scripts.js",
+    "/assets/img/bg-masthead.jpg",
+    "/assets/img/portfolio/fullsize/1.jpg",
+    "/assets/img/portfolio/fullsize/2.jpg",
+    "/assets/img/portfolio/fullsize/3.jpg",
+    "/assets/img/portfolio/fullsize/4.jpg",
+    "/assets/img/portfolio/fullsize/5.jpg",
+    "/assets/img/portfolio/fullsize/6.jpg",
+    "/assets/img/portfolio/thumbnails/1.jpg",
+    "/assets/img/portfolio/thumbnails/2.jpg",
+    "/assets/img/portfolio/thumbnails/3.jpg",
+    "/assets/img/portfolio/thumbnails/4.jpg",
+    "/assets/img/portfolio/thumbnails/5.jpg",
+    "/assets/img/portfolio/thumbnails/6.jpg",
 ]
 
 self.addEventListener("install", installEvent => {
